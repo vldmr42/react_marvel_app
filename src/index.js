4de602a10444dcb24d153c0f3183748c5cf55571
manemejeff <kmb14056@yandex.ru>
2022-02-06 20:43:41 +0400

    added simple base for character requests

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
+import MarvelService from './services/MarvelService';
+
 import './style/style.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const marvelService = new MarvelService();
 
+marvelService.getAllCharacters().then(res => res.data.results.forEach(item=>console.log(item.name)));
+
+ReactDOM.render( 
+    <React.StrictMode >
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
